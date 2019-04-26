c1f8c28a4be947852e56e7aa919ae9499e3efb72
Ade Adesina <janet.adesina@gmail.com>
2019-04-26 17:56:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,9 +15,26 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  lowercase = 
-  if (string.toLowerCase === lowercase)
-  return "I can\t hear you!"
-  // string.toLowerCase
-  // string =
+  if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
+  if (string.toLowerCase()) {
+    return "I can\'t hear you!"
+  }
+  else if (string.toUpperCase()) {
+     return "YES INDEED!"
+  }
 }
+
+// function sayHiToGrandma(string) {
+//   if (string.toLowerCase()) {
+//     return "I can\'t hear you!"
+//   }
+//   else if (string.toUpperCase()) {
+//     return "YES INDEED!"
+//   }
+//   else if (string === "I love you, Grandma.") {
+//     return "I love you, too"
+//   }
+// }
+
